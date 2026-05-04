962ec8053019c458bc4c6c3930184057781c0468
Justin <s4003900@student.rmit.edu.au>
2026-05-04 15:52:19 +1000

    Adjusted key parameters and usage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from crypto import mod_inverse 
 from node import Node
-import inventoryA, inventoryB, inventoryC, inventoryD
+import keys_config 
 
 # Build nodes and public keys from inventory data
 def build_node(inventory):
@@ -18,7 +18,11 @@ def build_node(inventory):
 nodes = []
 public_keys = {}
 
-for inventory in [inventoryA, inventoryB, inventoryC, inventoryD]:
+for inventory in [keys_config.INVENTORY_A_KEYS,
+                  keys_config.INVENTORY_B_KEYS,
+                  keys_config.INVENTORY_C_KEYS,
+                  keys_config.INVENTORY_D_KEYS]:
+    
     node, public_key = build_node(inventory)
     nodes.append(node)
     public_keys[inventory.name] = public_key
